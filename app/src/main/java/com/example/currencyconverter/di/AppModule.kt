b04c3cda75ec07f4c8befe09ad61ae58c41d0643
Nicolás <nicolasmlambertucci@gmail.com>
2021-02-15 19:21:07 -0300

    added network and improved dagger

---
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.example.currencyconverter.di
+
+import com.example.currencyconverter.data.network.CurrencyAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL ="https://exchangeratesapi.io/"
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(): CurrencyAPI = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CurrencyAPI::class.java)
+}
